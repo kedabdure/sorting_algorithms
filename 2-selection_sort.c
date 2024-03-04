d2f24a7e354a969bf052ca596cbf23c206a9d81b
root <root@DESKTOP-1O2F9DA.localdomain>
2024-03-04 11:36:53 +0300

    creat 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min, tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			tmp = array[min];
+			array[min] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
+	}
+}
