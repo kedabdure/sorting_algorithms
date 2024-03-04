014c44e44d8e74f23b27cc20936e66c50d2a20e3
root <root@DESKTOP-1O2F9DA.localdomain>
2024-03-04 09:44:59 +0300

    update file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,14 +7,16 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+    listint_t *temp, *current;
+    
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-
-	listint_t *current = (*list)->next;
+    
+	current = (*list)->next;
 
 	while (current != NULL)
 	{
-		listint_t *temp = current;
+		temp = current;
 
 		while (temp->prev != NULL && temp->n < temp->prev->n)
 		{
